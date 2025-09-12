7973a67e4bc744ad7450d3414241bed8e882c7a5
Jean Caetano <caetanojean4@gmail.com>
2025-09-12 16:21:07 -0300

    FEAT: Atualizando dependencias e aplicando decoradores para documentação swagger

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
 import {
   IsDate,
@@ -9,17 +10,33 @@ import {
 } from 'class-validator';
 
 export class CreateUserDto {
+  @ApiProperty({
+    description: 'O nome completo do usuário.',
+    example: 'Jean Caetano',
+  })
   @IsString()
   @IsNotEmpty({ message: 'O nome é obrigatório.' })
   name: string;
 
+  @ApiProperty({
+    description: 'O endereço de e-mail único do usuário.',
+    example: 'jean.teste@email.com',
+  })
   @IsEmail({}, { message: 'Por favor, insira um e-mail válido.' })
   email: string;
 
+  @ApiPropertyOptional({
+    description: 'O número de telefone do usuário (opcional), incluindo DDD.',
+    example: '85999999999',
+  })
   @IsPhoneNumber('BR', { message: 'O número de telefone fornecido não é válido.'})
   @IsOptional()
   telefone: string;
 
+  @ApiPropertyOptional({
+    description: 'A data de nascimento do usuário (opcional), no formato YYYY-MM-DD.',
+    example: '2004-01-17',
+  })
   @IsOptional()
   @Type(() => Date)
   @IsDate()
